59e3192c3cea7c7bd54f95207bb80c46b08e957f
Boro <boroousseni7@gmail.com>
2022-02-22 17:15:23 +0100

    Responsive ifr.php, participants link, add online file

--- access/online.php
+++ access/online.php
@@ -1,13 +1,13 @@
 <?php 
-    session_start();
+    // session_start();    
     $email=$_SESSION['email'];
-    $session_times=15;
+    $session_times=2*60*60;
     $current_times=date('U');
     $user_ip=$_SERVER['REMOTE_ADDR'];
     // $user_ip="17002-555";
-    include('../account/connectToDB.php');
-    $sql="SELECT * FROM online WHERE user_ip='$user_ip'";
-    $request=mysqli_query($bdd,$sql);
+    
+    $sql_online="SELECT * FROM online WHERE user_ip='$user_ip'";
+    $request=mysqli_query($bdd,$sql_online);
     $table=mysqli_fetch_assoc($request);
     if($table){
         $sql_update="UPDATE online SET times=$current_times WHERE user_ip=$user_ip";
@@ -23,9 +23,11 @@
     $sql_delete="DELETE FROM online WHERE times < $session_delete_time";
     $request_delete=mysqli_query($bdd,$sql_delete);
 
-    $sql="SELECT count(id) as nbr FROM online WHERE 1";
-    $request=mysqli_query($bdd,$sql);
-    $users_nbr_online=mysqli_fetch_assoc($request);
+    $sql_online="SELECT count(id) as nbr FROM online WHERE 1";
+    $request=mysqli_query($bdd,$sql_online);
+    $tab=mysqli_fetch_assoc($request);
+    $users_nbr_online=$tab['nbr'];
+    echo $users_nbr_online;
         
 
 ?>
