cc9a6cafcbf9a7dc735e91f5c884b2b513628065
Boro <boroousseni7@gmail.com>
2022-02-23 01:03:40 +0100

    Second commit

--- account/connectToDB.php
+++ account/connectToDB.php
@@ -1,3 +1,4 @@
 <?php
-$bdd = mysqli_connect('localhost', 'root', '', 'sicom_innovation') or die(mysqli_error($bdd));
+// $bdd = mysqli_connect('localhost', 'root', '', 'sicom_innovation') or die(mysqli_error($bdd));
+$bdd = mysqli_connect('us-cdbr-east-05.cleardb.net', 'bdd759e7c05277', '59d13ed0', 'heroku_fd9d17ca928ca6d') or die(mysqli_error($bdd));
 ?>
