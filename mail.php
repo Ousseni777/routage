7de3298e23786ee5d309fbc835078e38893453dd
Boro <boroousseni7@gmail.com>
2022-03-03 02:40:13 +0100

    Add email file

--- mail.php
+++ mail.php
@@ -15,13 +15,11 @@ $mail->isSMTP();                                            //Send using SMTP
 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 $mail->Username   = 'boro7ousseni@gmail.com';                     //SMTP username
-$mail->Password   = 'Orob-2022-s';                               //SMTP password
+$mail->Password   = '';                               //SMTP password
 $mail->Port       = "465";                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 $mail->SMTPSecure=PHPMailer::ENCRYPTION_SMTPS;
 
 //Recipients
-
-
 
 
 //Content
